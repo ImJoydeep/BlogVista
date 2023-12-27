236abf8fb8b3e6a891c392020233e2e4b6fa3aac
Joydeep Baidya <jbaidya42@gmail.com>
2023-12-27 23:46:26 +0530

    Update views.py

--- BlogVista/views.py
+++ BlogVista/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from models import Post, Comment
+from app.models import Post, Comment
 from rest_framework.views import APIView
-from models import Post, Comment
 from rest_framework.response import Response
 from django.core.paginator import Paginator
 from django.db.models import Q
-from serializers import PostSerializer
+from app.serializers import PostSerializer
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin
 from rest_framework import status
 
 
@@ -16,10 +18,21 @@ class PublicAPIView(APIView):
             blogs = Post.objects.all().order_by('?')
             if request.GET.get('search'):
                 search = request.GET.get('search')
-                blogs = blogs.filter(Q(title__icontains = search) | Q(blog_text__icontains = search))
+                blogs1 = Post.objects.filter(Q(title__icontains = search))
+                blogs2 = Post.objects.filter(Q(content__icontains = search))
+                blogs = blogs1.union(blogs2)
+                page_number = request.GET.get('page', 1)
+                paginator = Paginator(blogs, 10)
+            
+                serializer = PostSerializer(paginator.page(page_number), many=True)
+                response = {
+                "results": serializer.data,
+                "message": "blog successfully"
+                }
+                return Response(response, status=status.HTTP_200_OK)
                 
             page_number = request.GET.get('page', 1)
-            paginator = Paginator(blogs, 1)
+            paginator = Paginator(blogs, 10)
             
             serializer = PostSerializer(paginator.page(page_number), many=True)
             response = {
@@ -29,3 +42,10 @@ class PublicAPIView(APIView):
             return Response(response, status=status.HTTP_200_OK)
         except Exception as e:
             return Response(repr(e), status=status.HTTP_400_BAD_REQUEST)
+        
+class createBlog(GenericAPIView, CreateModelMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
